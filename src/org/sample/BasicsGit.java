0bbe8c6cd18e0908b577a0d7abc77a0e531510e1
Dinesh2520 <dineshshanmugam2520@gmail.com>
2023-07-26 19:49:03 +0530

    FirstProject

---
+++ src/org/sample/BasicsGit.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class BasicsGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I'm the new project");
+	}
+
+}
